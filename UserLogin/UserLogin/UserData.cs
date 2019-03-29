434bcd08142cbd64b31a99afdb273d72576542a9
nellidimitrowa <nellidimitrowa@gmail.com>
2019-03-29 16:46:17 +0200

    Modified TestUser from UserData to TestUsers (array) and added more users

--- UserLogin/UserLogin/UserData.cs
+++ UserLogin/UserLogin/UserData.cs
@@ -8,16 +8,18 @@ namespace UserLogin
 {
     static class UserData
     {
-        private static User _testUser;
-        public static User TestUser
+        private static User[] _testUsers;
+        public static User[] TestUsers
         {
-            get { resetTestUserData(); return _testUser; }
+            get { resetTestUserData(); return _testUsers; }
             set { }
         }
 
         private static void resetTestUserData()
         {
-            _testUser = new User("nedyalka", "password", "121212605", 1);
+            _testUsers[0] = new User("nedyalka", "password", "121212605", 1);
+            _testUsers[1] = new User("boyana", "password", "125456456", 4);
+            _testUsers[2] = new User("dimitar", "password", "12132155", 4);
         }
     }
 }
