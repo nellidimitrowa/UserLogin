907be19159fbfd6ec5f45e343c72b4dd3292e3e5
nellidimitrowa <nellidimitrowa@gmail.com>
2019-04-15 08:46:46 +0300

    Added Student.cs in StudentRepository

---
+++ UserLogin/StudentRepository/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRepository
+{
+    class Student
+    {
+        string firstName;
+        string secondName;
+        string lastName;
+        string faculty;
+        string speciality;
+        string educationalDegree;
+        string status;
+        string facultyNumber;
+        int course;
+        int stream;
+        int group;
+
+        Student() { }
+        Student(string firstName, string secondName, string lastName, string faculty, string speciality, string educationalDegree,
+            string status, string facultyNumber, int course, int stream, int group)
+        {
+            this.firstName = firstName;
+            this.secondName = secondName;
+            this.lastName = lastName;
+            this.faculty = faculty;
+            this.speciality = speciality;
+            this.educationalDegree = educationalDegree;
+            this.status = status;
+            this.facultyNumber = facultyNumber;
+            this.course = course;
+            this.stream = stream;
+            this.group = group;
+        }
+
+        public override string ToString()
+        {
+            return "Names: " + firstName + " " + secondName + " " + lastName + "\nFaculty: " + faculty + "\nSpeciality: " + speciality +
+                "\nEducational degree: " + educationalDegree + "\nStatus: " + status + "\nFaculty number: " + facultyNumber +
+                "\nCourse: " + course + "\nStream: " + stream + "\nGroup: " + group;
+        }
+    }
+}
