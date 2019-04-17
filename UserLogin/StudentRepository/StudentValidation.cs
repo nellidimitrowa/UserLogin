ca9058134ec6c892ba92a6a2c87b9e8d9b0cee64
nellidimitrowa <nellidimitrowa@gmail.com>
2019-04-17 18:55:27 +0300

    Added GetStudentDataByUser in StudentValidation.cs

---
+++ UserLogin/StudentRepository/StudentValidation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserLogin;
+
+namespace StudentRepository
+{
+    class StudentValidation
+    {
+        public Student GetStudentDataByUser (User user) 
+        {
+            if(user.facultyNumber == null)
+            {
+                Console.WriteLine("There is no student with this user's faculty number");
+                return null;
+            }
+            else
+            {
+                Student student = StudentData.FindStudentByFacultyNumber(user.facultyNumber);
+                if(student == null)
+                {
+                    Console.WriteLine("There is no student with this user's faculty number");
+                    return null;
+                }
+               return student;
+            }
+        }
+    }
+}
