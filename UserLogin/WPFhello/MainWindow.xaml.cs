5c8e0ddeefe5a893beeeab611664a2233ffd3be7
nellidimitrowa <nellidimitrowa@gmail.com>
2019-04-22 21:28:02 +0300

    Added view form to StudentInfoSystem.cs

--- UserLogin/WPFhello/MainWindow.xaml.cs
+++ UserLogin/WPFhello/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace WPFhello
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
